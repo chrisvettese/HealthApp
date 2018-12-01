bda6e987b1397f91515c1e2d5fdf7e06d282a09a
Chris Vettese <cvett018@uottawa.ca>
2018-12-01 16:33:13 -0500

    wow this code is messy

--- app/src/main/java/io/github/chrisvettese/healthapp/Client.java
+++ app/src/main/java/io/github/chrisvettese/healthapp/Client.java
@@ -5,17 +5,30 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Client {
     private static String name;
     private static int doctorID;
 
+    private static boolean bookingMode = false;
+    private static boolean viewingMode = false;
+
+    private static List<String> dates;
+
     public static void setup(final MainActivity activity) {
+        dates = new ArrayList<>();
         activity.findViewById(R.id.viewAppointmentsClient).setVisibility(View.GONE);
+
+        activity.findViewById(R.id.dateInput).setVisibility(View.GONE);
+        activity.findViewById(R.id.timeInput).setVisibility(View.GONE);
+        activity.findViewById(R.id.submitClient).setVisibility(View.GONE);
         try {
             FileInputStream nameIn = activity.openFileInput("id.txt");
             StringBuilder builder = new StringBuilder();
@@ -29,16 +42,27 @@ public class Client {
             //Client hasn't entered their doctor's id yet
             Client.setupIDInput(activity);
         }
+        //Back button
         activity.findViewById(R.id.backButtonClient).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                activity.setContentView(R.layout.activity_main);
-                MainActivity.initMainActivity(activity);
+                if (!bookingMode) {
+                    activity.setContentView(R.layout.activity_main);
+                    MainActivity.initMainActivity(activity);
+                } else {
+                    bookingMode = false;
+                    activity.findViewById(R.id.dateInput).setVisibility(View.GONE);
+                    activity.findViewById(R.id.timeInput).setVisibility(View.GONE);
+                    activity.findViewById(R.id.submitClient).setVisibility(View.GONE);
+
+                    activity.findViewById(R.id.viewAppointmentsClient).setVisibility(View.VISIBLE);
+                    activity.findViewById(R.id.requestClient).setVisibility(View.VISIBLE);
+                }
             }
         });
     }
 
-    public static void setupIDInput(MainActivity activity) {
+    public static void setupIDInput(final MainActivity activity) {
         final TextInputLayout idInput = activity.findViewById(R.id.idInput);
         idInput.getEditText().setSingleLine();
         idInput.getEditText().setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -47,13 +71,83 @@ public class Client {
                 if (actionID == EditorInfo.IME_ACTION_DONE) {
                     doctorID = Integer.parseInt(idInput.getEditText().getText().toString());
                     //Check if doctorID is valid
-                    return true;
+                    boolean validID = NetworkClient.validateID(doctorID);
+                    //Correct ID entered: hide this input and show all buttons
+                    if (validID) {
+                        idInput.setVisibility(View.GONE);
+                        Button viewButton = activity.findViewById(R.id.viewAppointmentsClient);
+                        viewButton.setVisibility(View.VISIBLE);
+                        Button requestClient = activity.findViewById(R.id.requestClient);
+                        requestClient.setVisibility(View.VISIBLE);
+
+                        //Add listener for requesting appointments
+                        requestClient.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Client.setRequestView(activity);
+                            }
+                        });
+                        //Add listener for viewing appointments
+                        viewButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Client.setAppointmentView(activity);
+                            }
+                        });
+                        return true;
+                    }
                 }
                 return false;
             }
         });
     }
-    public static void setName(String name) {
+    protected static void setName(String name) {
         Client.name = name;
+    }
+    protected static void addAppointment(String date) {
+        dates.add(date);
+    }
+    /**When the "Book Appointment" button is pressed*/
+    protected static void setRequestView(final MainActivity a) {
+        bookingMode = true;
+
+        final TextInputLayout dateInput = a.findViewById(R.id.dateInput);
+        final TextInputLayout timeInput = a.findViewById(R.id.timeInput);
+        final Button submitClient = a.findViewById(R.id.submitClient);
+
+        dateInput.setVisibility(View.VISIBLE);
+        timeInput.setVisibility(View.VISIBLE);
+        submitClient.setVisibility(View.VISIBLE);
+
+        a.findViewById(R.id.viewAppointmentsClient).setVisibility(View.GONE);
+        a.findViewById(R.id.requestClient).setVisibility(View.GONE);
+        //When patient clicks submit to book appointment, verify they entered valid input and send to doctor
+        submitClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String date = dateInput.getEditText().getText().toString();
+                String time = timeInput.getEditText().getText().toString();
+                //If input for date and time
+                if (date.length() == 8 && time.length() == 4) {
+                    //Send appointment request to doctor
+                    NetworkClient.sendAppointmentRequest(date, time);
+                    //Disable booking mode
+                    bookingMode = false;
+                    dateInput.setVisibility(View.GONE);
+                    timeInput.setVisibility(View.GONE);
+                    submitClient.setVisibility(View.GONE);
+
+                    a.findViewById(R.id.viewAppointmentsClient).setVisibility(View.VISIBLE);
+                    a.findViewById(R.id.requestClient).setVisibility(View.VISIBLE);
+                } else {
+                    dateInput.getEditText().setText("");
+                    timeInput.getEditText().setText("");
+                }
+            }
+        });
+    }
+    /**When patient wants to view booked appointments*/
+    protected static void setAppointmentView(final MainActivity a) {
+        
     }
 }
--- app/src/main/java/io/github/chrisvettese/healthapp/NetworkClient.java
+++ app/src/main/java/io/github/chrisvettese/healthapp/NetworkClient.java
@@ -1,4 +1,88 @@
 package io.github.chrisvettese.healthapp;
 
-public class NetworkClient {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class NetworkClient extends Service {
+    //Tell doctor to check ID
+    public static final byte CHECK_ID = (byte) -200;
+    //Tell doctor about requested appointment time; Tell client about appointment time
+    public static final byte BOOK = (byte) -199;
+
+    private Socket clientSocket;
+    private static ObjectInputStream in;
+    private static ObjectOutputStream out;
+    private boolean open = true;
+
+    @Override
+    public void onCreate() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    clientSocket = new Socket("localhost", 2468);
+                    in = new ObjectInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+                    out = new ObjectOutputStream(new BufferedOutputStream(clientSocket.getOutputStream()));
+
+                    while (open) {
+                        //Receive update from doctor
+                        byte dataIn = in.readByte();
+                        if (dataIn == BOOK) {
+                            //Read the date from doctor as string
+                            String date = in.readUTF();
+                            Client.addAppointment(date);
+                        }
+                    }
+                } catch (IOException e) {
+                    //Either client is disconnected or server isn't open yet
+                    //Do nothing for now
+                }
+            }
+        });
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    @Override
+    public void onDestroy() {
+        open = false;
+        try {
+            if (clientSocket != null) clientSocket.close();
+            if (in != null) in.close();
+            if (out != null) out.close();
+        } catch (IOException e) {
+
+        }
+    }
+
+    protected static boolean validateID(int ID) {
+        try {
+            out.writeByte(CHECK_ID);
+            out.writeInt(ID);
+            return in.readBoolean();
+        } catch (IOException e) {
+            //oops
+        }
+        return false;
+    }
+    protected static void sendAppointmentRequest(String date, String time) {
+        try {
+            out.writeByte(BOOK);
+            out.writeUTF(date);
+            out.writeUTF(time);
+        } catch (IOException e) {
+            //oh no
+        }
+    }
 }
